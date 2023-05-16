f5144590c1178a5d17b9a4df91e25dbc55691d44
GalalMohammed <galalmoh2001@gmail.com>
2023-05-16 04:37:54 +0300

    edit none cond

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None or sentence == "":
+    if sentence is None or sentence == "":
         fc = None
         sentence_len = 0
     else:
