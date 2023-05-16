3070bd3ba07ed047f3d2c2a5e445c25e8e4fe048
GalalMohammed <galalmoh2001@gmail.com>
2023-05-16 04:21:16 +0300

    fix for list range

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for idx, j in enumerate(matrix[i]):
+        for idx, j in enumerate(i):
             print("{:d}".format(j), end="")
-            if not idx == len(matrix[i]) - 1:
+            if not idx == len(i) - 1:
                 print(" ", end="")
-        print()
-    if matrix == [[]]:
         print()
