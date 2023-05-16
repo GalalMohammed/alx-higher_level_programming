d08aba22c5dc6cf37a78ec0922b7aa3d3e36d647
GalalMohammed <galalmoh2001@gmail.com>
2023-05-16 04:37:09 +0300

    add task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == None or sentence == "":
+        fc = None
+        sentence_len = 0
+    else:
+        fc = sentence[0]
+        sentence_len = len(sentence)
+    return (sentence_len, fc)
