18143222bdb91d8079942e1a7a6af9d657fdfaa4
GalalMohammed <galalmoh2001@gmail.com>
2023-05-19 22:22:02 +0300

    handle None parameter

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary.values()):
+    if a_dictionary and len(a_dictionary.values()):
         best = max(a_dictionary.values())
         for key in a_dictionary.keys():
             if a_dictionary[key] == best:
