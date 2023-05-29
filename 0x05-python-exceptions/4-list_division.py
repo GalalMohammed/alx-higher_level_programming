bd2707a2414bdb87f7b66214ee53e54819fbb4c5
GalalMohammed <galalmoh2001@gmail.com>
2023-05-30 00:09:16 +0300

    try to print out of range seeral times

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     res = []
-    try:
-        for i in range(list_length):
-            try:
-                res.append(my_list_1[i] / my_list_2[i])
-            except TypeError:
-                print("wrong type")
-                res.append(0)
-            except ZeroDivisionError:
-                print("division by 0")
-                res.append(0)
-    except IndexError:
-        print("out of range")
-    finally:
-        res.extend([0 for i in range(list_length - len(res))])
-        return res
+    for i in range(list_length):
+        try:
+            res.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            res.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            res.append(0)
+        except IndexError:
+            print("out of range")
+            res.append(0)
+        finally:
+            pass
+    return res
