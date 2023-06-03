9c43804a09e28d4bf9907e150c87c2a0be4139c0
GalalMohammed <galalmoh2001@gmail.com>
2023-06-03 11:28:51 +0300

    add task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square Docstring.
+
+This module defines an empty class Square.
+Example:
+    You can define a square::
+
+        my_square = Square()
+        print(my_square.__dict__)
+
+"""
+
+
+class Square:
+    """Empty class defines a square.
+
+    """
+    pass
