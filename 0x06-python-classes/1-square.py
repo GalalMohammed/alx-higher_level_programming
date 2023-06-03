50e9aabfb979a780a8b2f3f020a6bfd9196ff3bc
GalalMohammed <galalmoh2001@gmail.com>
2023-06-03 11:41:26 +0300

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square Docstring.
+
+This module defines a class Square.
+
+Example:
+    You can define a square::
+
+        my_square = Square(3)
+
+"""
+
+
+class Square:
+    """A class defines a square.
+
+    """
+    def __init__(self, size):
+        """__init__ method.
+
+        Args:
+            size (float): The size of square.
+
+        """
+        self.__size = size
