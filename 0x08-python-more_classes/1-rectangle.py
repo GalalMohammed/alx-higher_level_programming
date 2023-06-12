b2eb62313690061a63a904bb5980f023c4287735
GalalMohammed <galalmoh2001@gmail.com>
2023-06-12 14:57:02 +0300

    fix code style

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,7 +15,6 @@ Example:
 class Rectangle(object):
     """Class defines a rectangle."""
 
-
     def __init__(self, width=0, height=0):
         """__init__ method.
 
@@ -27,12 +26,10 @@ class Rectangle(object):
         self.width = width
         self.height = height
 
-
     @property
     def width(self):
         """:obj:`int`: rectangle width."""
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -42,12 +39,10 @@ class Rectangle(object):
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
         """:obj:`int`: rectangle height."""
         return self.__height
-
 
     @height.setter
     def height(self, value):
