08f62a73d0202cdf1581106234b84301d53743ee
GalalMohammed <galalmoh2001@gmail.com>
2023-06-21 03:37:33 +0300

    update

--- 0x0C-python-almost_a_circle/tests/base_test.py
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -12,14 +12,14 @@ class TestBase(unittest.TestCase):
         self.b2 = Base(12)
 
     def test_id_value(self):
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 12)
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 12)
 
     def test_to_json(self):
         self.assertEqual(Base.to_json_string([{'x': 0, 'y': 0}]),
                 '[{"x": 0, "y": 0}]')
         self.assertEqual(Base.to_json_string([]), '[]')
-        self.assertEqual(Base.to_json_string(), '[]')
+        self.assertEqual(Base.to_json_string(None), '[]')
 
 if __name__ == '__main__':
     unittest.main()
