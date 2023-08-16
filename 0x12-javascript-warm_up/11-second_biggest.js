0daa631ab6db07fa9e28fc9eee6b06277cea865b
GalalMohammed <galalmoh2001@gmail.com>
2023-08-16 20:06:55 +0300

    parse args to int

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length < 4) { console.log(0); } else {
   const arr = []; let max; let max2;
-  for (let i = 0; i < process.argv.length - 2; i++) { arr[i] = process.argv[i + 2]; }
+  for (let i = 0; i < process.argv.length - 2; i++) { arr[i] = parseInt(process.argv[i + 2]); }
   if (arr[0] > arr[1]) {
     max = arr[0];
     max2 = arr[1];
