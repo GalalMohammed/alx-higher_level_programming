0adb04f80275e9ee8aeed0917366a40832f27abc
GalalMohammed <galalmoh2001@gmail.com>
2023-08-21 13:51:19 +0300

    eliminate port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ This module lists all states from the database hbtn_0e_0_usa.
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     numrows = cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
