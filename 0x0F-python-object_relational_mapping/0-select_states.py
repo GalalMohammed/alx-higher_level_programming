830a6a42c4ecbfd5173875f8e7eac0c0e40d943d
GalalMohammed <galalmoh2001@gmail.com>
2023-08-21 13:46:38 +0300

    add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module lists all states from the database hbtn_0e_0_usa.
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
