24b6814e5cacad50bfd8be03439ee3a11056603c
GalalMohammed <galalmoh2001@gmail.com>
2023-08-21 19:03:30 +0300

    add task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-This module lists all states from the database hbtn_0e_0_usa.
+This module lists all states with a name starting with N
 
 Example:
-    $ ./0-select_states.py root root hbtn_0e_0_usa
+    $ ./1-filter_states.py root root hbtn_0e_0_usa
 
 """
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    numrows = cur.execute("SELECT * FROM states ORDER BY id")
+    numrows = cur.execute("SELECT * FROM states ORDER BY id 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
