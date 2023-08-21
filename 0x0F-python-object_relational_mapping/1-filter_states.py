4f8d1d2d437db625464b9a93a5769f6eedfede0f
GalalMohammed <galalmoh2001@gmail.com>
2023-08-21 19:30:55 +0300

    fix sql statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    numrows = cur.execute("SELECT * FROM states ORDER BY id 'N%'")
+    cur.execute("SELECT * FROM states ORDER BY id WHERE name LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
