67658ee01ac6a0266b5b34252778752149798171
GalalMohammed <galalmoh2001@gmail.com>
2023-08-22 19:23:36 +0300

    fix line break

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name
-            FROM cities, states WHERE cities.state_id=states.id
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities, states WHERE cities.state_id=states.id\
             ORDER BY ctates.id")
     rows = cur.fetchall()
     for row in rows:
