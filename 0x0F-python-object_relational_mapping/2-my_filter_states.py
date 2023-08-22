45d881de1cc77a91060f06785d7276cb786fe817
GalalMohammed <galalmoh2001@gmail.com>
2023-08-22 18:17:57 +0300

    add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+
+Example:
+    $ ./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'
+
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name={} ORDER BY id".format(
+                sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
