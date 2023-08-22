9fbb5798c5f76b2e18442232c677deef9cd5f388
GalalMohammed <galalmoh2001@gmail.com>
2023-08-22 18:59:09 +0300

    add task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+
+Example:
+    $ ./3-my_safe_filter_states.py root root hbtn_0e_0_usa 'Arizona'
+
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='%s' ORDER BY id",
+                (sys.argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
