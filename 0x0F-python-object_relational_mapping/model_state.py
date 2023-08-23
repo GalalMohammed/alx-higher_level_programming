e4f20d8934f3ba13922c4a0683b10e9abcb90e31
GalalMohammed <galalmoh2001@gmail.com>
2023-08-23 19:15:49 +0300

    fix table name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,6 +16,6 @@ Base = declarative_base()
 
 class State(Base):
     """class inherits from Base."""
-    __tablename__ = 'state'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
