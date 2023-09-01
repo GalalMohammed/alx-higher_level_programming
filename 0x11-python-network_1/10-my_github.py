cef338b1dc128cfcb27d14757545fb0f152039e3
GalalMohammed <galalmoh2001@gmail.com>
2023-09-02 01:30:44 +0300

    add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module takes your GitHub credentials (username and password)
+to display your id.
+
+Example:
+    $ ./10-my_github.py papamuziko cisfun
+
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = 'https://api.github.com/users/' + sys.argv[1]
+    headers = {
+            'Accept': "application/vnd.github+json",
+            'Authorization': f'Bearer {sys.argv[2]}',
+            'X-GitHub-Api-Version': '2022-11-28'
+            }
+    r = requests.get(url, headers=headers)
+    print(r.json()['id'])
