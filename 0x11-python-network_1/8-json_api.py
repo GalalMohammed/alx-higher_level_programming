0b8f747c73967990a0c1217ddfd66b387ca9d12c
GalalMohammed <galalmoh2001@gmail.com>
2023-09-02 00:22:15 +0300

    add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+Example:
+    $ ./8-json_api.py a
+
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    if len(sys.argv) > 1:
+        payload = {'q': sys.argv[1]}
+    else:
+        payload = {'q': ""}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    if r.json():
+        print(f"[{r.json()[id]}] {r.json()[name]}")
+    else:
+        print("No result")
