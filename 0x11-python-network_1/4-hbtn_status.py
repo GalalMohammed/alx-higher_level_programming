1d091a9d86834ca2b8cd891312bca36bd366caee
GalalMohammed <galalmoh2001@gmail.com>
2023-09-01 23:43:21 +0300

    add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module fetches https://alx-intranet.hbtn.io/status
+
+Example:
+    $ ./4-hbtn_status.py
+
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
