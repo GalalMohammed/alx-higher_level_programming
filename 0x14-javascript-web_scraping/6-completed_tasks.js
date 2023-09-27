506e3760dc1593dc6296fb52ae849196d1b88d90
GalalMohammed <galalmoh2001@gmail.com>
2023-09-28 02:13:00 +0300

    fix standards

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,15 +3,15 @@ const request = require('request');
 
 // Send a GET request to the specified URL
 request.get(process.argv[2], (_err, _response, body) => {
-  const completed_tasks = {};
-  for (task of JSON.parse(body)) {
+  const completedTasks = {};
+  for (const task of JSON.parse(body)) {
     if (task.completed) {
-      if (completed_tasks[task.userId]) {
-        completed_tasks[task.userId] += 1;
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId] += 1;
       } else {
-        completed_tasks[task.userId] = 1;
+        completedTasks[task.userId] = 1;
       }
     }
   }
-  console.log(completed_tasks);
+  console.log(completedTasks);
 });
