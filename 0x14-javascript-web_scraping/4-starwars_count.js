aeb1a399fec4148cafdef2533c05dfd603132d45
GalalMohammed <galalmoh2001@gmail.com>
2023-09-28 01:47:51 +0300

    make it more generic

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request.get(process.argv[2], (_err, _response, body) => {
   let count = 0;
   for (const i of JSON.parse(body).results) {
     for (const j of i.characters) {
-      if (j === process.argv[2].slice(-process.argv[2].length, -5) + 'people/18/') { count += 1; }
+      if (j.includes('18')) { count += 1; }
     }
   }
   console.log(count);
