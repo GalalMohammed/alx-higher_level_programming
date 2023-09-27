9d19835af7fe7156717e4f4134eef259885fc4d1
GalalMohammed <galalmoh2001@gmail.com>
2023-09-28 00:27:40 +0300

    add task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+
+// Read the file asynchronously in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
