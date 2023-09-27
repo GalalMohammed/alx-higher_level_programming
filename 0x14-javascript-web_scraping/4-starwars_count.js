5f53fbdc0e25e185914508699e23351abb3cc491
GalalMohammed <galalmoh2001@gmail.com>
2023-09-28 01:16:40 +0300

    add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Send a GET request to Star wars API
+request.get(process.argv[2], (_err, _response, body) => {
+  let count = 0;
+  for (const i of JSON.parse(body).results) {
+    for (const j of i.characters) {
+      if (j === 'https://swapi-api.alx-tools.com/api/people/18/') { count += 1; }
+    }
+  }
+  console.log(count);
+});
